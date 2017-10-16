733b9417b4d3ecc396c385b1b15f77c20b19d3fa
Ondrej Hejda <ondrejh.ck@email.cz>
2017-10-16 22:14:16 +0200

    buffering, waiting timeout

--- tiva/project.c
+++ tiva/project.c
@@ -12,9 +12,27 @@
 #define LED_GREEN_OFF() do{ROM_GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3,0);}while(0)
 #define LED_GREEN_SWAP() do{ROM_GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3,ROM_GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_3)^GPIO_PIN_3);}while(0)
 
+#define UART1_AVAIL (UARTCharsAvail(UART1_BASE))
+#define UART2_AVAIL (UARTCharsAvail(UART2_BASE))
+#define UART1_GETC() (UARTCharGet(UART1_BASE))
+#define UART2_GETC() (UARTCharGet(UART2_BASE))
+
 #define OUTBUFLEN 256
+#define INBUFLEN 256
+
+
+void uart_printf(char *buf);
+void uart_write_hex(uint32_t val,int len);
+void uart_write_int(uint32_t val);
+
 uint8_t outbuf[OUTBUFLEN];
 int outbptri = 0, outbptro = 0;
+
+uint8_t inbuf1[INBUFLEN], inbuf2[INBUFLEN];
+uint32_t inbptri1 = 0, inbptri2 = 0, inbptro1 = 0, inbptro2 = 0;
+uint32_t t_start1, t_start2;
+uint32_t t_last1, t_last2;
+bool busy1, busy2;
 
 uint32_t g_ui32SysClock;
 
@@ -27,6 +45,75 @@ void uart_output_poll(void)
         c = outbuf[outbptro++];
         ROM_UARTCharPut(UART0_BASE,c);
         outbptro%=OUTBUFLEN;
+    }
+}
+
+void uart_input1_poll(void)
+{
+    if (UART1_AVAIL) {
+        uint8_t c = UART1_GETC();
+        inbuf1[inbptri1++] = c;
+        inbptri1 %= INBUFLEN;
+        t_last1 = g_loop_us;
+        if (!busy1) {
+            busy1 = true;
+            LED_GREEN_ON();
+            t_start1 = g_loop_ms;
+        }
+    }
+    else {
+        if (busy1) {
+            if ((g_loop_us-t_last1)>((20*1000000)/9600)) { // end of rx detected
+                uart_write_hex(t_start1,4);
+                uart_printf(" U1:");
+                int cnt = 0;
+                while (inbptri1!=inbptro1) {
+                    if ((cnt++&0x01)==0)
+                        uart_printf(" ");
+                    uart_write_hex(inbuf1[inbptro1++],2);
+                    inbptro1 %= INBUFLEN;
+                }
+                busy1 = false;
+                uart_printf("\n\r");
+                LED_GREEN_OFF();
+            }
+        }
+    }
+}
+
+void uart_input2_poll(void)
+{
+    if (UART2_AVAIL) {
+        uint8_t c = UART2_GETC();
+        inbuf2[inbptri2++] = c;
+        inbptri2 %= INBUFLEN;
+        t_last2 = g_loop_us;
+        if (!busy2) {
+            busy2 = true;
+            LED_RED_ON();
+            t_start2 = g_loop_ms;
+        }
+    }
+    else {
+        if (busy2) {
+            if ((g_loop_us-t_last2)>((20*1000000)/9600)) { // end of rx detected
+                uart_write_hex(t_start2,4);
+                uart_printf(" U2:");
+                uart_printf(" (");
+                uart_write_int((inbptri2-inbptro2)%INBUFLEN);
+                uart_printf(")");
+                int cnt = 0;
+                while (inbptri2!=inbptro2) {
+                    if ((cnt++&0x01)==0)
+                        uart_printf(" ");
+                    uart_write_hex(inbuf2[inbptro2++],2);
+                    inbptro2 %= INBUFLEN;
+                }
+                busy2 = false;
+                uart_printf("\n\r");
+                LED_RED_OFF();
+            }
+        }
     }
 }
 
@@ -67,11 +154,6 @@ void uart_input2_init(void)
     //ROM_GPIOPinConfigure(GPIO_PD7_U2TX);
     ROM_GPIOPinTypeUART(GPIO_PORTD_BASE, GPIO_PIN_6);// | GPIO_PIN_7);
 }
-
-#define UART1_AVAIL (UARTCharsAvail(UART1_BASE))
-#define UART2_AVAIL (UARTCharsAvail(UART2_BASE))
-#define UART1_GETC() (UARTCharGet(UART1_BASE))
-#define UART2_GETC() (UARTCharGet(UART2_BASE))
 
 void uart_putc(uint8_t c)
 {
@@ -121,7 +203,21 @@ void uart_write_hex(uint32_t val,int len)
     }
     str[l] = '\0';
 
-    uart_write(str, l);
+    uart_printf((char*)str);
+}
+
+void uart_write_int(uint32_t val)
+{
+    uint8_t str[11];
+    uint8_t ptr = 9;
+    str[10] = '\0';
+    uint32_t v = val;
+    do {
+        str[ptr--] = (v%10)+'0';
+        v/=10;
+    } while (v>0);
+    ptr++;
+    uart_printf((char*)&str[ptr]);
 }
 
 void init_board(void)
@@ -169,11 +265,6 @@ int main(void)
     uart_input1_init();
     uart_input2_init();
 
-    int lastRx = 0;
-    int cnt = 0;
-
-    //uart_printf("ahoj\n\r");
-
     // Loop forever.
     while(1) {
         g_loop_ms = millis();
@@ -182,37 +273,7 @@ int main(void)
         ROM_GPIOPinWrite(GPIO_PORTA_BASE,GPIO_PIN_4,ROM_GPIOPinRead(GPIO_PORTA_BASE,GPIO_PIN_3)?0:GPIO_PIN_4);
 
         uart_output_poll();
-
-        if (UART1_AVAIL) {
-            if (lastRx!=1) {
-                if (lastRx!=0)
-                    uart_write((uint8_t *)"\n\r", 2);
-                uart_write((uint8_t *)"U1:", 3);
-                cnt = 0;
-                lastRx = 1;
-            }
-            if (!(cnt&0x01))
-                uart_write((uint8_t *)" ", 1);
-            uart_write_hex(UART1_GETC(), 2);
-            cnt++;
-            LED_RED_ON();
-            LED_GREEN_OFF();
-        }
-
-        if (UART2_AVAIL) {
-            if (lastRx!=2) {
-                if (lastRx!=0)
-                    uart_write((uint8_t *)"\n\r", 2);
-                uart_write((uint8_t *)"U2:", 3);
-                cnt = 0;
-                lastRx = 2;
-            }
-            if (!(cnt&0x01))
-                uart_write((uint8_t *)" ", 1);
-            uart_write_hex(UART2_GETC(), 2);
-            cnt++;
-            LED_RED_OFF();
-            LED_GREEN_ON();
-        }
+        uart_input1_poll();
+        uart_input2_poll();
     }
 }
